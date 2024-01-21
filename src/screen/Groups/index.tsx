e3368e17875586a368fbd8d44d0860b9ad6b43f5
Igor Wiepieski <iw.programacao@gmail.com>
2024-01-21 19:45:07 -0300

    Inicio do projeto

---
+++ src/screen/Groups/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { View, Text } from 'react-native';
+export function Groups() {
+  return (
+    <View>
+      <Text>Eu namoro um pitelzinho</Text>
+    </View>
+  )
+}
