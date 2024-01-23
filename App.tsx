0b6da2d5897707614b957edeff16bf632018ea2c
Igor Wiepieski <iw.programacao@gmail.com>
2024-01-23 19:09:39 -0300

    Path mapping nativo

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { StyleSheet,  View } from 'react-native';
-import { Groups } from './src/screen/Groups';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   return (
