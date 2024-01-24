2bb990367d9bf541370740880f43e72fad825892
Igor Wiepieski <iw.programacao@gmail.com>
2024-01-24 10:19:42 -0300

    Propriedades do Flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
@@ -17,6 +18,10 @@ export function Groups() {
         data={groups} // Fonte de dados
         keyExtractor={(item) => item} // Recomendado valor único para identificação e performance (ex: id)
         renderItem={({ item }) => <GroupCard title={item} />} // Conteúdo para renderizar
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )} // Renderização de lista vazia
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
     </Container>
   );
